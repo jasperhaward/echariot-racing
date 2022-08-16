786c9f1910ba367e44690e6344e2b615fc4b94c5
Jasper Haward <jasperhaward@virginmedia.com>
2022-08-16 14:17:39 +0200

    feat: redesign of home page

---
+++ public/js/components/nav.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("a[role='navlink']");
+
+  for (const navLink of navLinks) {
+    navLink.onclick = (event) => {
+      event.preventDefault();
+
+      const href = navLink.getAttribute("href");
+
+      document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+    };
+  }
+});
--- src/app.js
+++ src/app.js
@@ -13,55 +13,93 @@ app.set("views", path.resolve("./src/views"));
 app.set("view engine", "pug");
 app.use(express.static("public"));
 
+const events = [
+  {
+    name: "Hickstead European Championship",
+    url: "www.google.com",
+    location: {
+      url: "https://www.google.hr/maps/place/Reigate/",
+      address: "City Hall",
+      town: "Reigate",
+      county: "Surrey",
+    },
+    date: "1st - 4th August 2022",
+  },
+  {
+    name: "Silverstone GP",
+    url: "www.google.com",
+    location: {
+      url: "https://www.google.hr/maps/place/Birmingham/",
+      address: "NEC",
+      town: "Birmingham",
+      county: "Surrey",
+    },
+    date: "16th - 20th September 2022",
+  },
+  {
+    name: "Cow Pie",
+    url: "www.google.com",
+    location: {
+      url: "https://www.google.hr/maps/place/Birmingham/",
+      address: "Field",
+      town: "Le town",
+      county: "Warwickshire",
+    },
+    date: "7th - 9th October 2022",
+  },
+];
+
 // Routes
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        backgroundImage: "/images/racing/BC_Image01.jpg",
-        activeRoute: req.path,
-    });
+  res.render("test", {
+    backgroundImage: "/images/racing/BC_Image06_Cropped.jpg",
+    galleryImages: fs
+      .readdirSync("public/images/racing")
+      .map((fileName) => `/images/racing/${fileName}`),
+    events,
+  });
 });
 
-app.get("/test", (req, res) => {
-    res.render("test", {
-        backgroundImage: "/images/racing/BC_Image06_Cropped.jpg",
-        galleryImages: fs
-            .readdirSync("public/images/racing")
-            .map((fileName) => `/images/racing/${fileName}`),
-        activeRoute: req.path,
-    });
+/*
+app.get("/", (req, res) => {
+  res.render("home", {
+    backgroundImage: "/images/racing/BC_Image01.jpg",
+    activeRoute: req.path,
+  });
 });
 
 app.get("/events", (req, res) => {
-    res.render("events", {
-        backgroundImage: "/images/racing/BC_Image03.jpg",
-        activeRoute: req.path,
-    });
+  res.render("events", {
+    backgroundImage: "/images/racing/BC_Image03.jpg",
+    activeRoute: req.path,
+  });
 });
 
 app.get("/gallery", (req, res) => {
-    res.render("gallery", {
-        backgroundImage: "/images/racing/BC_Image06.jpg",
-        galleryImages: fs
-            .readdirSync("public/images/racing")
-            .map((fileName) => `/images/racing/${fileName}`),
-        activeRoute: req.path,
-    });
+  res.render("gallery", {
+    backgroundImage: "/images/racing/BC_Image06.jpg",
+    galleryImages: fs
+      .readdirSync("public/images/racing")
+      .map((fileName) => `/images/racing/${fileName}`),
+    activeRoute: req.path,
+  });
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact", {
-        backgroundImage: "/images/racing/RN_Image01_Edited.jpg",
-        activeRoute: req.path,
-    });
+  res.render("contact", {
+    backgroundImage: "/images/racing/RN_Image01_Edited.jpg",
+    activeRoute: req.path,
+  });
 });
 
 app.use("*", (req, res) => {
-    res.status(404).render("404");
+  res.status(404).render("404");
 });
+*/
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
 
 export default app;
