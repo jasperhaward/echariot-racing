30924d3d3cb0f2678c3ff720eabbce40ee0c1676
Jasper Haward <jasperhaward@virginmedia.com>
2022-08-17 16:34:47 +0200

    feat: sticky scrollar

--- public/js/components/nav.js
+++ public/js/components/nav.js
@@ -6,8 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault();
 
       const href = navLink.getAttribute("href");
+      const target = document.querySelector(href);
 
-      document.querySelector(href).scrollIntoView({
+      const rect = target.getBoundingClientRect();
+
+      window.scrollTo({
+        top: window.pageYOffset + rect.top - 50, // window scroll Y pos + element top - nav height
         behavior: "smooth",
       });
     };
